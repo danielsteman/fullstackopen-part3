4fb15312df626aebb0be90c30b597011604043d8
Daniel Steman <daniel-steman@live.nl>
2020-12-10 11:28:31 +0100

    included mongoose in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors')
 const Person = require('./models/person')
 const e = require('express')
