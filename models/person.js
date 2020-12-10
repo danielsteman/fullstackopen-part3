9a0df6a08be6e6d0b200c677e2bb517f6c249ed5
Daniel Steman <daniel-steman@live.nl>
2020-12-10 14:50:09 +0100

    update

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
 require('dotenv').config()
 
 const url = process.env.MONGODB_URI
@@ -12,10 +13,18 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: Number,
-    date: Date,
+    name: {
+        type: String,
+        unique: true,
+        minlength: 3
+    },
+    number: {
+        type: Number,
+        minlength: 8
+    }
 })
+
+personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
